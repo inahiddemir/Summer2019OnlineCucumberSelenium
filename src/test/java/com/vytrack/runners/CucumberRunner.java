2d30b3edbeb3e2c2dcfc8c045cb7591abac926b9
inahiddemir <i.nahid.demir@gmail.com>
2019-12-18 01:37:18 -0800

    ADDED REPORTS

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src\\test\\resources\\features",    //path to features
         glue = "com\\vytrack\\step_definitions", //path to step definitions
-        dryRun = false,  //if you make it true, it will not run your test, just check you have code for each test step
+        dryRun = true,  //if you make it true, it will not run your test, just check you have code for each test step
         tags = "",//whatever you put inside tags, only that test cases will run with has that tag
         plugin = {"html:target/default-cucumber-reports"} //default report from cucumber
         )
